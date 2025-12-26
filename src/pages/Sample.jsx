572c0460de4cfaa0f7b97c3baeed28d15a08f9f6
croissen214 <croissen214@gmail.com>
2025-12-26 18:43:27 +0900

    auto deploy

---
+++ src/pages/Sample.jsx
@@ -0,0 +1,10 @@
+import * as S from "./Sample.styles";
+
+export default function Home() {
+
+  return (
+    <S.Container>
+      <S.Title>샘플</S.Title>
+    </S.Container>
+  );
+}
---
+++ src/pages/Sample.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  background: linear-gradient(135deg, #ffb6c1, #ffd6e8);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 10px;
+`;
+
